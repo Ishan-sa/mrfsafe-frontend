e0903cc8854f06f2f9fa10e72f945f1e518dcd6f
Ishan-sa <ishansach19@gmail.com>
2024-03-14 12:18:20 -0700

    more styling

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -5,58 +5,60 @@ import Link from "next/link";
 
 export default function LoginForm() {
   return (
-    <>
-      <div className="flex just items-center h-screen bg-login-background w-full bg-cover relative">
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="absolute inset-0 backdrop-blur-md"></div>
-        <div className="flex justify-center items-center max-h-[600px] m-auto w-full h-full max-w-[1170px] relative z-10">
-          <div className="w-full h-full bg-white flex flex-col justify-around">
-            <div className="flex px-6">
-              <Image
-                src={"/images/mobilecomm-logo.png"}
-                alt="mrfsafe-logo"
-                width={100}
-                height={100}
-                className="min-w-[120px]"
-              />
-            </div>
-            <div className="flex justify-center">
-              <Image
-                src={"/images/mRFSafe-logo.svg"}
-                alt="mrfsafe-logo"
-                width={100}
-                height={100}
-                className="min-w-[300px]"
-              />
-            </div>
+    <div className="flex items-center justify-center h-screen w-full bg-login-background bg-cover bg-no-repeat relative">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="absolute inset-0 backdrop-blur-md"></div>
 
-            <div className="bg-login-background-2 w-full h-full flex justify-center items-center bg-cover">
-              <div className="flex gap-6 text-[12px] items-center justify-center">
-                <Link
-                  href="https://www.mcpsinc.com/privacy-policy"
-                  target="_blank"
-                >
-                  Privacy Policy
-                </Link>
-                <p>Copyright © MobileComm Professionals Inc 2023</p>
-              </div>
-            </div>
+      <div className="flex justify-center items-center h-full w-full max-w-[1170px] relative z-10 flex-col py-16 px-8 md:flex-row">
+        {/* Left Side */}
+        <div className="w-full md:w-1/2 h-full flex flex-col justify-between bg-white">
+          <div className="flex justify-center md:justify-start px-6">
+            <Image
+              src="/images/logos/mobilecomm-logo.png"
+              alt="MobileComm Logo"
+              width={120}
+              height={120}
+              className="object-contain"
+            />
           </div>
-          <div className="bg-[#2393b9] w-full h-full p-6">
-            <div className="flex flex-col justify-center items-center h-full">
-              <div className="text-container mb-10">
-                <h1 className="text-white text-2xl font-bold">
-                  Welcome to mRFSafe
-                </h1>
-                <p className="text-[#b6b7b7]">
-                  Sign in by entering information below
-                </p>
-                <Form />
-              </div>
+          <div className="flex justify-center">
+            <Image
+              src="/images/logos/mRFSafe-logo.svg"
+              alt="mRFSafe Logo"
+              width={300}
+              height={300}
+              className="object-contain"
+            />
+          </div>
+          <div className="bg-login-background-2 w-full h-full flex justify-center items-center bg-cover bg-no-repeat">
+            <div className="flex justify-center items-center text-sm text-center gap-6 px-6 mb-4 md:mb-0">
+              <Link
+                href="https://www.mcpsinc.com/privacy-policy"
+                target="_blank"
+              >
+                Privacy Policy
+              </Link>
+              <p className="text-black text-sm">
+                Copyright © MobileComm Professionals Inc 2023
+              </p>
             </div>
           </div>
         </div>
+
+        {/* Right Side */}
+        <div className="w-full md:w-1/2 h-full bg-[#2393b9] p-6 flex flex-col justify-center items-center">
+          <div className="mb-10">
+            <h1 className="text-white text-2xl font-bold">
+              Welcome to mRFSafe
+            </h1>
+            <p className="text-[#b6b7b7]">
+              Sign in by entering information below
+            </p>
+            <Form />
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
